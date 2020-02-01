301569c459d7401090cbbb9eebee107116995274
Cainie <karol.marszalik@gmail.com>
2020-02-02 00:27:00 +0100

    Added non moving pedestrians

---
+++ KrakJam2020/Assets/Scripts/Obstacle/Pedestrian.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Obstacle;
+using UnityEngine;
+
+public class Pedestrian : RoadObstacle {
+	
+	[SerializeField] private List<GameObject> victimModels;
+	
+	private void Awake(){
+		LoadRandomVictimModel();
+	}
+
+	private void LoadRandomVictimModel(){
+		var randomCarModel = victimModels[Random.Range(0, victimModels.Count)];
+		var instantiatedCarModel = Instantiate(randomCarModel, gameObject.transform);
+	}
+	    
+}
