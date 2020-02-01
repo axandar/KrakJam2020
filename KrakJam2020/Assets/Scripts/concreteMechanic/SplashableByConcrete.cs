5610729f570fdab397827a0793b7cc57c19085d4
Michał Piątkowski <axandar29@gmail.com>
2020-02-01 22:28:29 +0100

    WIP spawning hole

--- KrakJam2020/Assets/Scripts/concreteMechanic/SplashableByConcrete.cs
+++ KrakJam2020/Assets/Scripts/concreteMechanic/SplashableByConcrete.cs
@@ -1,21 +1,33 @@
 using System;
 using highScore;
+using Obstacle;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace concreteMechanic{
-	[RequireComponent(typeof(Rigidbody))]
-	public class SplashableByConcrete:MonoBehaviour{
+	[RequireComponent(typeof(Rigidbody), typeof(RoadObstacle))]
+	public class SplashableByConcrete : MonoBehaviour{
 		[SerializeField] bool isSplashed;
 		[SerializeField] int scoreGainedBySplashing;
 		[SerializeField] HighScore highScore;
+		[SerializeField] private SpriteRenderer spriteRenderer;
+		[SerializeField] private Sprite filledHole;
+		
+		private RoadObstacle _roadObstacle;
+
+		private void Start(){
+			_roadObstacle = GetComponent<RoadObstacle>();
+			highScore = _roadObstacle.highScore;
+		}
 
 		public void SplashByConcrete(){
 			if(isSplashed){
 				return;
 			}
-			
+
 			isSplashed = true;
+			spriteRenderer.sprite = filledHole;
+			_roadObstacle.enabled = false;
 			highScore.AddScore(scoreGainedBySplashing);
 		}
 	}
