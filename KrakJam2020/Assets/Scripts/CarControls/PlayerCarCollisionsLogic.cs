a9a7c16efdc992415510f01574566fcbaa4fe528
Kajetan Jóźwiak <unvios22@gmail.com>
2020-02-01 11:38:29 +0100

    Add obstacle type collision cooldown

--- KrakJam2020/Assets/Scripts/CarControls/PlayerCarCollisionsLogic.cs
+++ KrakJam2020/Assets/Scripts/CarControls/PlayerCarCollisionsLogic.cs
@@ -1,25 +1,29 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class PlayerCarCollisionsLogic : MonoBehaviour {
 	public UnityEvent<Obstacle> obstacleCollidedEvent;
 	
-	[SerializeField] float obstacleCollisionCooldownTime;
-	
-	bool _isObstacleCollisionsOnCooldown;
+	[SerializeField] float obstacleTypeCollisionIgnoreTime;
+
+	List<Obstacle.ObstacleType> _currentlyIgnoredObstacleTypes;
 
 	void OnTriggerEnter(Collider other) {
 		var obstacleScript = other.GetComponent<Obstacle>();
-		if (obstacleScript != null && !_isObstacleCollisionsOnCooldown) {
-			obstacleCollidedEvent.Invoke(obstacleScript);
-			obstacleScript.ObstacleCollidedWithPlayer();
-			ReactToObtacleCollision(other, obstacleScript.obstacleType);
-		}
+		if (obstacleScript == null) {return;}
+		var collidedObstacleType = obstacleScript.obstacleType;
+		if(_currentlyIgnoredObstacleTypes.Contains(collidedObstacleType)) { return; }
+		
+		obstacleCollidedEvent.Invoke(obstacleScript);
+		obstacleScript.ObstacleCollidedWithPlayer();
+		ReactToObstacleCollision(other, collidedObstacleType );
+		StartCoroutine(TempIgnoreObstacleType(collidedObstacleType, obstacleTypeCollisionIgnoreTime));
 	}
 
-	void ReactToObtacleCollision(Collider other, Obstacle.ObstacleType obstacleType) {
+	void ReactToObstacleCollision(Collider other, Obstacle.ObstacleType obstacleType) {
 		switch (obstacleType) {
 			default:
 				throw new ArgumentNullException(obstacleType.ToString(),"obstacleType not recognized!");
@@ -27,10 +31,10 @@ public class PlayerCarCollisionsLogic : MonoBehaviour {
 		//TODO: add car behaviour
 	}
 
-	IEnumerator ObstacleCollisionsCooldown() {
-		_isObstacleCollisionsOnCooldown = true;
-		yield return new WaitForSeconds(obstacleCollisionCooldownTime);
-		_isObstacleCollisionsOnCooldown = false;
+	IEnumerator TempIgnoreObstacleType(Obstacle.ObstacleType ignoredObstacleType, float obstacleTypeIgnoreTime) {
+		_currentlyIgnoredObstacleTypes.Add(ignoredObstacleType);
+		yield return new WaitForSeconds(obstacleTypeIgnoreTime);
+		_currentlyIgnoredObstacleTypes.Remove(ignoredObstacleType);
 	}
 }
 
