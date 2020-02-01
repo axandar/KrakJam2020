01b6bd7922602f340a1ef8b5b94d8ed84dd6d67a
Kajetan Jóźwiak <unvios22@gmail.com>
2020-02-01 02:45:16 +0100

    Add Car rotation on left-right movement

--- KrakJam2020/Assets/Scripts/CarControls/CarController.cs
+++ KrakJam2020/Assets/Scripts/CarControls/CarController.cs
@@ -1,9 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class CarController : MonoBehaviour {
-	[SerializeField] float playerSpeed;
-	[SerializeField] float maxYOffset;
-	[SerializeField] float maxXOffset;
+	public Action playerTurningEffectsStartedEvent;
+	public Action playerTurningEffectsStoppedEvent;
+	
+	[SerializeField] float playerMoveSpeed;
+	[SerializeField] float playerRotationChangeSpeed;
+	[SerializeField] float playerRotationMaxValue;
+	
+	float _playerRotationAngle;
 
 	Vector2 _playerInput;
 
@@ -25,9 +31,37 @@ public class CarController : MonoBehaviour {
 
 	void FixedUpdate() {
 		ApplyPlayerInput();
+		UpdateCarRotationAngle();
+		ApplyCarRotationAngle();
+		Debug.log(_rigi);
 	}
 
 	void ApplyPlayerInput() {
-		_rigidbody.AddForce(_playerInput * (playerSpeed * Time.fixedDeltaTime), ForceMode.Impulse);
+		_rigidbody.AddForce(_playerInput * (playerMoveSpeed * Time.fixedDeltaTime), ForceMode.Impulse);
+	}
+	
+	void UpdateCarRotationAngle(){
+		if (_playerInput.x > 0) {
+			_playerRotationAngle -= Time.fixedDeltaTime * playerRotationChangeSpeed * 3;
+		}else if (_playerInput.x < 0) {
+			_playerRotationAngle += Time.fixedDeltaTime * playerRotationChangeSpeed * 3;
+		} else {
+			_playerRotationAngle = Mathf.MoveTowards(_playerRotationAngle, 0f,
+			 Time.fixedDeltaTime * playerRotationChangeSpeed);
+		}
+		ClampCarRotationAngle();
+	}
+	
+	void ClampCarRotationAngle() {
+		if (_playerRotationAngle > playerRotationMaxValue) {
+			_playerRotationAngle = playerRotationMaxValue;
+		} else if (_playerRotationAngle < -playerRotationMaxValue) {
+			_playerRotationAngle = -playerRotationMaxValue;
+		}
+	}
+
+	void ApplyCarRotationAngle() {
+		var currentRbRotation = _rigidbody.rotation.eulerAngles;
+		_rigidbody.rotation = Quaternion.Euler(currentRbRotation.x, currentRbRotation.y, _playerRotationAngle);
 	}
 }
