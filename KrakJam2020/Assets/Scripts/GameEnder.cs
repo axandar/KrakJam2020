e12232900c4025bab00d54d154161999d416efe6
Kajetan Jóźwiak <unvios22@gmail.com>
2020-02-02 13:01:14 +0100

    WIP: add game ender

---
+++ KrakJam2020/Assets/Scripts/GameEnder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameEnder : MonoBehaviour {
+    [SerializeField] int gameLengthInSeconds;
+    int _remainingGameTime;
+
+    void Start() {
+        _remainingGameTime = gameLengthInSeconds;
+        StartCoroutine(CoundownRemainingGameTime());
+    }
+
+    IEnumerator CoundownRemainingGameTime() {
+        while (_remainingGameTime > 0) {
+            yield return new WaitForSeconds(1f);
+            _remainingGameTime -= 1;
+        }
+        EndGame();
+    }
+    
+    void EndGame() {
+        //TODO: end game, load game ended scene
+    }
+
+}
